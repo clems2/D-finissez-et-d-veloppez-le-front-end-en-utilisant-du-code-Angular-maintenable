23d92b131485bef3dcf60b34fe7363a5c6fece09
@clemcir <clementcirou22@gmail.com>
2026-01-14 23:50:42 +0100

    feat : chartAdapter, chartFactory, countryDatas, countryService created

---
+++ src/app/adapters/chart-adapter.spec.ts
@@ -0,0 +1,7 @@
+import { ChartAdapter } from './chart-adapter';
+
+describe('ChartAdapter', () => {
+  it('should create an instance', () => {
+    expect(new ChartAdapter()).toBeTruthy();
+  });
+});
---
+++ src/app/models/country-datas.spec.ts
@@ -0,0 +1,7 @@
+import { CountryDatas } from './country-datas';
+
+describe('CountryDatas', () => {
+  it('should create an instance', () => {
+    expect(new CountryDatas()).toBeTruthy();
+  });
+});
