4c1ae623cb5c82dbe2e8756363294c8a25217e63
@clemcir <clementcirou22@gmail.com>
2026-01-16 16:59:24 +0100

    feat : implemented DataService

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,9 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, map, Observable, shareReplay } from 'rxjs';
+import { Olympic } from '../models/olympic';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+private readonly url = './assets/mock/olympic.json';
 
-  constructor() { }
+  constructor(private http:HttpClient) {}
+
+  getOlympic(): Observable<Olympic[]> {
+    return this.http.get<Olympic[]>(this.url).pipe(
+      shareReplay(1),
+    );
+  }
+  
+  getOlympicByCountry(countryName: string): Observable<Olympic>{
+    return this.getOlympic().pipe(
+      map(olympics => {
+        const olympic = olympics.find(o => o.country === countryName);
+          if (!olympic) {
+            throw new Error('Country not found');
+          }
+        return olympic;
+      }),
+    )
+  }
+
+
 }
