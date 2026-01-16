b1ee2ac7338941860a2cb7d316fdc6c51f240252
@clemcir <clementcirou22@gmail.com>
2026-01-16 16:12:34 +0100

    fix: name of the service fit with the spec

--- src/app/services/data.service.spec.ts
+++ src/app/services/data.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { CountryService } from './country.service';
+import { DataService } from './data.service';
 
-describe('CountryService', () => {
-  let service: CountryService;
+describe('DataService', () => {
+  let service: DataService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(CountryService);
+    service = TestBed.inject(DataService);
   });
 
   it('should be created', () => {
